f972cf512348f326be166afff8b8b95abb1f8fd3
Loi Van Tran <datloiboi@gmail.com>
2021-08-09 17:48:05 +0700

    Add a petition

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 
 const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
@@ -9,142 +8,27 @@ const mapStyles = {
   height: "100%",
 };
 
-class CurrentLocation extends React.Component {
-  constructor(props) {
-    super(props);
-
-    const { lat, lng } = this.props.initialCenter;
-
-    this.state = {
-      currentLocation: {
-        lat: lat,
-        lng: lng,
-      },
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.google !== this.props.google) {
-      this.loadMap();
-    }
-    if (prevState.currentLocation !== this.state.currentLocation) {
-      this.recenterMap();
-    }
-  }
-
-  recenterMap() {
-    const map = this.map;
-    const current = this.state.currentLocation;
-    const google = this.props.google;
-    const maps = google.maps;
-
-    if (map) {
-      let center = new maps.LatLng(current.lat, current.lng);
-      map.panTo(center);
-    }
-  }
-
-  componentDidMount() {
-    if (this.props.centerAroundCurrentLocation) {
-      if (navigator && navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((pos) => {
-          const coords = pos.coords;
-          this.setState({
-            currentLocation: {
-              lat: coords.latitude,
-              lng: coords.longitude,
-            },
-          });
-        });
-      }
-    }
-    this.loadMap();
-  }
-
-  loadMap() {
-    if (this.props && this.props.google) {
-      const { google } = this.props;
-      const maps = google.maps;
-
-      const mapRef = this.refs.map;
-
-      const node = ReactDOM.findDOMNode(mapRef);
-
-      let { zoom } = this.props;
-      const { lat, lng } = this.state.currentLocation;
-      const center = new maps.LatLng(lat, lng);
-
-      const mapConfig = Object.assign(
-        {},
-        {
-          center: center,
-          zoom: zoom,
-        },
-      );
-
-      this.map = new maps.Map(node, mapConfig);
-    }
-  }
-
-  renderChildren() {
-    const { children } = this.props;
-
-    if (!children) return;
-
-    return React.Children.map(children, (c) => {
-      if (!c) return;
-
-      return React.cloneElement(c, {
-        map: this.map,
-        google: this.props.google,
-        mapCenter: this.state.currentLocation,
-      });
-    });
-  }
-
-  render() {
-    const style = Object.assign({}, mapStyles.map);
-
-    return (
-      <div>
-        <h1>Current Location</h1>
-        <div style={style} ref="map">
-          Loading map...
-        </div>
-        {this.renderChildren()}
-      </div>
-    );
-  }
-}
-
-CurrentLocation.defaultProps = {
-  zoom: 14,
-  initialCenter: {
-    lat: -1.2884,
-    lng: 36.8233,
-  },
-  centerAroundCurrentLocation: false,
-  visible: true,
-};
-
-const spots = [
+const petitions = [
   {
-    type: "request",
+    type: "receive",
     lat: 10.797914587330832,
     lng: 106.71326158912827,
+    items: [
+      { type: 'rice', quantity: 1, kg: 1 },
+    ],
   },
   {
-    type: "request",
+    type: "receive",
     lat: 10.801024084611207,
     lng: 106.69901655713389,
   },
   {
-    type: "request",
+    type: "receive",
     lat: 10.802791798164124,
     lng: 106.71699088213137,
   },
   {
-    type: "drive",
+    type: "receive",
     lat: 10.792185360807064,
     lng: 106.70875174642686,
   },
@@ -184,7 +68,7 @@ export class MapContainer extends Component {
           lng: 106.71969978644816,
         }}
       >
-        {spots.map((s) => {
+        {petitions.map((s) => {
           return (
             <Marker
               name={s.type}
