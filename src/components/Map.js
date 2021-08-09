a1b87d1ebdc6489736d8b4bb75a7c45472cd71c5
Loi Van Tran <datloiboi@gmail.com>
2021-08-09 17:38:18 +0700

    Map working with some default HCMC default

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
-
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import ReactDOM from "react-dom";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 
 const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
@@ -9,18 +9,200 @@ const mapStyles = {
   height: "100%",
 };
 
+class CurrentLocation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { lat, lng } = this.props.initialCenter;
+
+    this.state = {
+      currentLocation: {
+        lat: lat,
+        lng: lng,
+      },
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.google !== this.props.google) {
+      this.loadMap();
+    }
+    if (prevState.currentLocation !== this.state.currentLocation) {
+      this.recenterMap();
+    }
+  }
+
+  recenterMap() {
+    const map = this.map;
+    const current = this.state.currentLocation;
+    const google = this.props.google;
+    const maps = google.maps;
+
+    if (map) {
+      let center = new maps.LatLng(current.lat, current.lng);
+      map.panTo(center);
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.centerAroundCurrentLocation) {
+      if (navigator && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((pos) => {
+          const coords = pos.coords;
+          this.setState({
+            currentLocation: {
+              lat: coords.latitude,
+              lng: coords.longitude,
+            },
+          });
+        });
+      }
+    }
+    this.loadMap();
+  }
+
+  loadMap() {
+    if (this.props && this.props.google) {
+      const { google } = this.props;
+      const maps = google.maps;
+
+      const mapRef = this.refs.map;
+
+      const node = ReactDOM.findDOMNode(mapRef);
+
+      let { zoom } = this.props;
+      const { lat, lng } = this.state.currentLocation;
+      const center = new maps.LatLng(lat, lng);
+
+      const mapConfig = Object.assign(
+        {},
+        {
+          center: center,
+          zoom: zoom,
+        },
+      );
+
+      this.map = new maps.Map(node, mapConfig);
+    }
+  }
+
+  renderChildren() {
+    const { children } = this.props;
+
+    if (!children) return;
+
+    return React.Children.map(children, (c) => {
+      if (!c) return;
+
+      return React.cloneElement(c, {
+        map: this.map,
+        google: this.props.google,
+        mapCenter: this.state.currentLocation,
+      });
+    });
+  }
+
+  render() {
+    const style = Object.assign({}, mapStyles.map);
+
+    return (
+      <div>
+        <h1>Current Location</h1>
+        <div style={style} ref="map">
+          Loading map...
+        </div>
+        {this.renderChildren()}
+      </div>
+    );
+  }
+}
+
+CurrentLocation.defaultProps = {
+  zoom: 14,
+  initialCenter: {
+    lat: -1.2884,
+    lng: 36.8233,
+  },
+  centerAroundCurrentLocation: false,
+  visible: true,
+};
+
+const spots = [
+  {
+    type: "request",
+    lat: 10.797914587330832,
+    lng: 106.71326158912827,
+  },
+  {
+    type: "request",
+    lat: 10.801024084611207,
+    lng: 106.69901655713389,
+  },
+  {
+    type: "request",
+    lat: 10.802791798164124,
+    lng: 106.71699088213137,
+  },
+  {
+    type: "drive",
+    lat: 10.792185360807064,
+    lng: 106.70875174642686,
+  },
+];
+
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false, // Hides or shows the InfoWindow
+    activeMarker: {}, // Shows the active marker upon click
+    selectedPlace: {}, // Shows the InfoWindow to the selected place upon a marker
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+
+  onClose = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null,
+      });
+    }
+  };
+
   render() {
     return (
       <Map
-        google={this.props.google}
-        zoom={14}
+        zoom={16}
         style={mapStyles}
+        google={this.props.google}
         initialCenter={{
-          lat: 10.7469,
-          lng: 106.6763,
+          lat: 10.796143556994366,
+          lng: 106.71969978644816,
         }}
-      />
+      >
+        {spots.map((s) => {
+          return (
+            <Marker
+              onClick={this.onMarkerClick}
+              name={s.type}
+              position={{ lat: s.lat, lng: s.lng }}
+            />
+          );
+        })}
+        <InfoWindow
+          onClose={this.onClose}
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,64 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 
-import MapContainer from "../../components/Map";
-
-const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-
-const mapStyles = {
-  width: "100%",
-  height: "100%",
-};
-
-const defaultCurrentLocation = {
-  zoom: 14,
-  visible: true,
-  centerAroundCurrentLocation: false,
-  initialCenter: {
-    lat: 10.7469,
-    lng: 106.6763,
-  },
-};
+import Map from "../../components/Map";
 
 function HomePage(props) {
-  const [showInfo, setShowInfo] = useState(false);
-  const [activeMarker, setActiveMarker] = useState({});
-  const [selectedPlace, setSelectedPlace] = useState({});
-  const [currentLocation, setCurrentLocation] = useState(
-    defaultCurrentLocation,
-  );
-
-  const onMarkerClick = (props, marker, e) => {
-    setActiveMarker(marker);
-    setSelectedPlace(props);
-    setShowInfo(true);
-  };
-
-  const onClose = (props) => {
-    if (showInfo) {
-      setShowInfo(false);
-      setActiveMarker(null);
-    }
-  };
-
-  useEffect(() => {
-    if (navigator && navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((pos) => {
-        const { coords } = pos;
-        console.log({ coords });
-        setCurrentLocation({
-          currentLocation: {
-            lat: coords.latitude,
-            lng: coords.longitude,
-          },
-        });
-      });
-    }
-  }, []);
 
   return (
     <div>
-      <MapContainer />
+      <Map />
     </div>
   );
 }
