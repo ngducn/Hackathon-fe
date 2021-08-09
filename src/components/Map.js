d10da6d1dce33a85735d249ad732d023c2b2f9f7
Loi Van Tran <datloiboi@gmail.com>
2021-08-09 18:03:39 +0700

    Refactor to hooks

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 
 const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
@@ -13,9 +13,7 @@ const petitions = [
     type: "receive",
     lat: 10.797914587330832,
     lng: 106.71326158912827,
-    items: [
-      { type: 'rice', quantity: 1, kg: 1 },
-    ],
+    items: [{ type: "rice", quantity: 1, kg: 1 }],
   },
   {
     type: "receive",
@@ -34,61 +32,54 @@ const petitions = [
   },
 ];
 
-export class MapContainer extends Component {
-  state = {
-    infoWindow: false,
-    activeMarker: {},
-    selectedPlace: {},
+function MapContainer(props) {
+  const [selectedPetition, setSelectedPetition] = useState({});
+  const [activePetition, setActivePetition] = useState({});
+  const [infoWindow, setInfoWindow] = useState(false);
+
+  const onMarkerClick = (props, marker, e) => {
+    setSelectedPetition(props);
+    setActivePetition(marker);
+    setInfoWindow(true);
   };
 
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      infoWindow: true,
-    });
-
-  onClose = (props) => {
-    if (this.state.infoWindow) {
-      this.setState({
-        infoWindow: false,
-        activeMarker: null,
-      });
+  const onClose = (props) => {
+    if (infoWindow) {
+      setActivePetition(null);
+      setInfoWindow(false);
     }
   };
 
-  render() {
-    return (
-      <Map
-        zoom={16}
-        style={mapStyles}
-        google={this.props.google}
-        initialCenter={{
-          lat: 10.796143556994366,
-          lng: 106.71969978644816,
-        }}
+  return (
+    <Map
+      zoom={16}
+      style={mapStyles}
+      google={props.google}
+      initialCenter={{
+        lat: 10.796143556994366,
+        lng: 106.71969978644816,
+      }}
+    >
+      {petitions.map((s) => {
+        return (
+          <Marker
+            name={s.type}
+            onClick={onMarkerClick}
+            position={{ lat: s.lat, lng: s.lng }}
+          />
+        );
+      })}
+      <InfoWindow
+        onClose={onClose}
+        marker={activePetition}
+        visible={infoWindow}
       >
-        {petitions.map((s) => {
-          return (
-            <Marker
-              name={s.type}
-              onClick={this.onMarkerClick}
-              position={{ lat: s.lat, lng: s.lng }}
-            />
-          );
-        })}
-        <InfoWindow
-          onClose={this.onClose}
-          marker={this.state.activeMarker}
-          visible={this.state.infoWindow}
-        >
-          <div style={{ height: 300, width: 300, backgroundColor: 'red'}}>
-            <h4>{this.state.selectedPlace.name}</h4>
-          </div>
-        </InfoWindow>
-      </Map>
-    );
-  }
+        <div style={{ height: 300, width: 300, backgroundColor: "red" }}>
+          <h4>{selectedPetition.name}</h4>
+        </div>
+      </InfoWindow>
+    </Map>
+  );
 }
 
 export default GoogleApiWrapper({
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import Map from "../../components/Map";
 
 function HomePage(props) {
-
   return (
     <div>
       <Map />
