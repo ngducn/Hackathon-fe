6490bf9516c4d9ada4698eb471fbf39570008030
Loi Van Tran <datloiboi@gmail.com>
2021-08-10 15:29:56 +0700

    Add filtering options for petitions

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,26 +1,48 @@
 import React, { useState, useEffect } from "react";
+import { Form, Col, Row, ButtonGroup, Button, } from "react-bootstrap";
 
 import Map from "../../components/Map";
 
 const url = process.env.REACT_APP_BACKEND_API
 
 function HomePage() {
+  const [allPetitions, setAllPetitions] = useState([])
   const [petitions, setPetitions] = useState([])
+  const [selectedPetitionType, setSelectedPetitionType] = useState("receive");
+
 
   useEffect( () => {
     const fetchPetitions = async () => {
       const resp = await fetch(url + '/petitions')
       const json = await resp.json()
+      setAllPetitions(json)
       setPetitions(json)
     }
     fetchPetitions()
-    
   }, [])
+
+  const onSelectPetitionType = ({target: { value }}) => {
+    setSelectedPetitionType(value);
+    const filteredPetitions = allPetitions.filter(p => p.type === value)
+    setPetitions(filteredPetitions);
+  }
+
   return (
-    <div>
-      <div className="Hi"></div>
-      <Map petitions={petitions} />
-    </div>
+    <Row>
+      <Col md="3" className="p-1">
+        <Form className="m-3">
+          <Form.Control type="query" placeholder="Facemasks..." />
+        </Form>
+        <ButtonGroup aria-label="Basic example" className="m-3">
+          <Button onClick={onSelectPetitionType} value={'receive'} variant="danger">Requests</Button>
+          <Button onClick={onSelectPetitionType} value={'provide'} variant="info">Available</Button>
+          <Button onClick={onSelectPetitionType} value={'relived'} variant="success">Relived</Button>
+        </ButtonGroup>
+      </Col>
+      <Col md="9">
+        <Map petitions={petitions} />
+      </Col>
+    </Row>
   );
 }
 
