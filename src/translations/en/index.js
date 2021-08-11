b199a2c59040b75856fb788fb0fb9ade094f6e1f
Loi Van Tran <datloiboi@gmail.com>
2021-08-11 09:33:24 +0700

    Add i18n

---
+++ src/translations/en/index.js
@@ -0,0 +1,10 @@
+export const TRANSLATIONS_EN = {
+  codevid_relief: "Codevid Relief",
+  welcome: "Welcome to the tutorial",
+  requests: "Requests",
+  provided_items: "Provided Items",
+  complete_requests: "Requests Completed",
+  men: "Men",
+  female: "Female",
+  activity: "Activity",
+};
---
+++ src/translations/zh/index.js
@@ -0,0 +1,10 @@
+export const TRANSLATIONS_ZH = {
+  codevid_relief: "Codevid 救济",
+  welcome: "欢迎使用本教程",
+  requests: "要求",
+  provided_items: "提供的物品",
+  complete_requests: "请求已完成",
+  men: "男人",
+  female: "女性",
+  activity: "活动",
+};
